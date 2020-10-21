69caebaeb854974fa4217bd661c30f7890d7305b
JRC <jorgeconejerosrios@gmail.com>
2020-10-21 18:25:19 -0300

    Agregado de Opciones
    Al seleccionar una opción, se ejecutará algún script próximamente hecho

--- Streamlit/app.py
+++ Streamlit/app.py
@@ -1,8 +1,28 @@
 # -*- coding: utf-8 -*-
-#from ../Conexion import Database as db
-import pymongo as pym
+import sys
+sys.path.insert(1,'/covid-19/Conexión')
+#from Conexion import DataBase
+#import pymongo as pym
 import pandas as pd
 import streamlit as st
 
-st.title("-Dashboard-Covid-19")
+st.sidebar.title("-Dashboard-Covid-19")
+datos = st.sidebar.radio(
+    "",
+    ("Indicadores", "Positividad Test PCR(%)", "Comparacion Entre Comunas o Regiones")
+)
+#Si selecciona Indicadores
+if datos == "Indicadores":
+    #script.main()
+    st.sidebar.title("Indicadores")
+
+#Si selecciona Positividad Test PCR(%)
+if datos == "Positividad Test PCR(%)":
+    #script.main()
+    st.sidebar.title("Positividad Test PCR(%)")
+
+#Si selecciona Comparacion Entre Comunas o Regiones
+if datos == "Comparacion Entre Comunas o Regiones":
+    #script.main()
+    st.sidebar.title("Comparacion Entre Comunas o Regiones")
 
