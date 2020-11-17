a97b308394bbc1ca7a027bc8b058f235d976268a
JRC <jorgeconejerosrios@gmail.com>
2020-11-17 20:35:57 -0300

    Graficar (parte 2)
    Ajuste de columnas para graficar

--- Conexión/app.py
+++ Conexión/app.py
@@ -1,12 +1,24 @@
-# -*- coding: utf-8 -*-
+
 from Conexion import Database as db
 import pymongo as pym
 import pandas as pd
 import streamlit as st
+import numpy as np
+
+def get_data():
+	df = pd.read_json('CollectionQuerry.json')
+	df = df.sort_index()
+	return df
+
 
 #Inicializar nuestra conexión
 db.initialize('covid')
-#DataFrame
-df = pd.read_json('../Templates/CollectionQuerry.json',encoding='UTF-8')
-st.json(df)
+df = get_data()
+df = df.drop(['_id','Region','Codigo comuna','Codigo region'],axis=1) # Borra las columnas
+df['positividad'] = np.where(np.isnan(df['positividad']), 0, df['positividad']) # Cambia los nan por el float 0.0
+#df = pd.to_numeric('positividad', downcast='float')
+print(df)
+st.line_chart(df)
+
+
 
