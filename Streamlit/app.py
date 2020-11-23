bacbf5d1e77730af3551167a2880391e75fce042
Felipe Espinoza <felipe.espinoza2019@alu.uct.cl>
2020-11-23 15:56:02 -0300

    Revert "Fixeo al app.py"
    This reverts commit 663db41a5b99dc97847d8f7996950cb9f7a5a30d.

--- Streamlit/app.py
+++ Streamlit/app.py
@@ -23,10 +23,9 @@ def Graficos_Jorge():
 
 #Prueba de graficos con json
 def Graficos_Mario():
-    st.sidebar.title('Navegación')
     df_from_json = pd.read_json('../Json/PCR_Regional.json')
     df_from_json['positividad'] = np.where(np.isnan(df_from_json['positividad']), 0, df_from_json['positividad'])
-    #print(df_from_json[['Region', 'fecha', 'positividad']])
+    print(df_from_json[['Region', 'fecha', 'positividad']])
     st.write("""
     # From Json_PCR_Regional to Streamlit Table
     """
@@ -35,26 +34,14 @@ def Graficos_Mario():
     region = st.sidebar.multiselect("Elegir regiones",
                                     ["Atacama", "Ñuble", "Magallanes", "Arica y Parinacota", "Aysén", "Coquimbo",
                                      "Araucanía", "Los Lagos", "Los Ríos", "Magallanes", "Tarapacá", "Valparaíso",
-                                     "Biobío", "O’Higgins", "Maule", "Metropolitana","Todas las Regiones"])
+                                     "Biobío", "O’Higgins", "Maule", "Metropolitana"])
     for e in range (len(region)):
         st.write(df_from_json[['Region','positividad','fecha']].loc[(df_from_json['Region'] ==str(region[e])) & (df_from_json['positividad']>=0)])
     chart_data = pd.DataFrame(
         np.random.randn(20, len(region)), columns=region)
 
     st.line_chart(chart_data)
-    for i in range(len(region)):
-        if region[i]=="Todas las Regiones":
-            st.write("""
-   ━━━━-╮\n 
-╰┃ ┣▇━▇\n
- ┃ ┃  ╰━▅╮\n
- ╰┳╯ ╰━━┳╯F A S I L I T O\n
-  ╰╮ ┳━━╯  EL \n
- ▕▔▋ ╰╮╭━╮TUTORIAL\n
-╱▔╲▋╰━┻┻╮╲╱▔▔▔╲\n
-▏  ▔▔▔▔▔▔▔  O O┃\n
-╲╱▔╲▂▂▂▂╱▔╲▂▂▂╱\n
- ▏╳▕▇▇▕ ▏╳▕▇▇▕""")
+
     # Posibilida de graficar en base a un mapa pero faltaria latitud y longitud para las zonas
     # map_data = pd.DataFrame(
     #   np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
@@ -63,6 +50,7 @@ def Graficos_Mario():
     # st.map(map_data)
 
 Graficos_Mario()
+
 
 
 
